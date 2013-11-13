f6262f30709733c9bff70b8ce4be1fc487f9a26f
LLFRealms <support@llfrealms.com>
2013-11-12 19:33:31 -0500

    took out mod and admin stuff to clean up the code, if they don't know how to use the menus then they suck

--- LLFRankUp/src/com/llfrealms/LLFRankUp/LLFRankUp.java
+++ LLFRankUp/src/com/llfrealms/LLFRankUp/LLFRankUp.java
@@ -274,54 +274,6 @@ public final class LLFRankUp extends JavaPlugin
 		{
 			Bukkit.getServer().dispatchCommand(sender, "menu open corporal-e");
 		}
-		else if(perms.has(player, "moderator.s"))
-		{
-			sendMessage(sender, "&9You're already S LEVEL WHAT MORE DO YOU WANT?!?");
-		}
-		else if(perms.has(player, "moderator.a"))
-		{
-			Bukkit.getServer().dispatchCommand(sender, "menu open moderator-s");
-		}
-		else if(perms.has(player, "moderator.b"))
-		{
-			Bukkit.getServer().dispatchCommand(sender, "menu open moderator-a");
-		}
-		else if(perms.has(player, "moderator.c"))
-		{
-			Bukkit.getServer().dispatchCommand(sender, "menu open moderator-b");
-		}
-		else if(perms.has(player, "moderator.d"))
-		{
-			Bukkit.getServer().dispatchCommand(sender, "menu open moderator-c");
-		}
-		else if(perms.has(player, "moderator.e"))
-		{
-			Bukkit.getServer().dispatchCommand(sender, "menu open moderator-d");
-		}
-		else if(perms.has(player, "admin.s"))
-		{
-			sendMessage(sender, "&9You're already S LEVEL WHAT MORE DO YOU WANT?!?");
-		}
-		else if(perms.has(player, "admin.a"))
-		{
-			Bukkit.getServer().dispatchCommand(sender, "menu open admin-s");
-		}
-		else if(perms.has(player, "admin.b"))
-		{
-			Bukkit.getServer().dispatchCommand(sender, "menu open admin-a");
-		}
-		else if(perms.has(player, "admin.c"))
-		{
-			Bukkit.getServer().dispatchCommand(sender, "menu open admin-b");
-		}
-		else if(perms.has(player, "admin.d"))
-		{
-			Bukkit.getServer().dispatchCommand(sender, "menu open admin-c");
-		}
-		else if(perms.has(player, "admin.e"))
-		{
-			Bukkit.getServer().dispatchCommand(sender, "menu open admin-d");
-		}
 		else
 		{
 			Bukkit.getServer().dispatchCommand(sender, "menu open ranks");
